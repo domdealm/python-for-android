a1171e22dfef1540d8ad0a9acd53fbf10a7bce6b
domdealm <62488579+domdealm@users.noreply.github.com>
2021-09-13 16:28:05 -0300

    Update __init__.py
    Solves numpy dlopen error.

--- pythonforandroid/recipes/numpy/__init__.py
+++ pythonforandroid/recipes/numpy/__init__.py
@@ -23,37 +23,23 @@ class NumpyRecipe(CompiledComponentsPythonRecipe):
         join('patches', 'compiler_cxx_fix.patch'),
         ]
 
-    def _build_compiled_components(self, arch):
-        info('Building compiled components in {}'.format(self.name))
+    call_hostpython_via_targetpython = False
 
-        env = self.get_recipe_env(arch)
-        with current_directory(self.get_build_dir(arch.arch)):
-            hostpython = sh.Command(self.real_hostpython_location)
-            if self.install_in_hostpython:
-                shprint(hostpython, 'setup.py', 'clean', '--all', '--force', _env=env)
-            hostpython = sh.Command(self.hostpython_location)
-            shprint(hostpython, 'setup.py', self.build_cmd, '-v',
-                    _env=env, *self.setup_extra_args)
-            build_dir = glob.glob('build/lib.*')[0]
-            shprint(sh.find, build_dir, '-name', '"*.o"', '-exec',
-                    env['STRIP'], '{}', ';', _env=env)
-
-    def _rebuild_compiled_components(self, arch, env):
-        info('Rebuilding compiled components in {}'.format(self.name))
-
-        hostpython = sh.Command(self.real_hostpython_location)
-        shprint(hostpython, 'setup.py', 'clean', '--all', '--force', _env=env)
-        shprint(hostpython, 'setup.py', self.build_cmd, '-v', _env=env,
-                *self.setup_extra_args)
+    def get_recipe_env(self, arch=None, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        
+        env['LDFLAGS'] += ' -lm'
+        
+        return env
 
     def build_compiled_components(self, arch):
         self.setup_extra_args = ['-j', str(cpu_count())]
-        self._build_compiled_components(arch)
+        super().build_compiled_components(arch)
         self.setup_extra_args = []
 
     def rebuild_compiled_components(self, arch, env):
         self.setup_extra_args = ['-j', str(cpu_count())]
-        self._rebuild_compiled_components(arch, env)
+        super().rebuild_compiled_components(arch, env)
         self.setup_extra_args = []
 
 
